9cd1640f0bfd0516607d521faf519c7566cab030
jandershock <jordan.andershock@gmail.com>
2022-06-23 12:03:28 -0500

    Finished attributes chapter lesson

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogGo.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
-        public string Address { get; set; }
-        public int NeighborhoodId { get; set; }
+
+        [Phone]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 5)]
+        public string Address { get; set; }
+
+        [Required]
+        public int NeighborhoodId { get; set; }
+
+        public Neighborhood Neighborhood { get; set; }
 
         public List<Dog> DogList { get; set; }
 
