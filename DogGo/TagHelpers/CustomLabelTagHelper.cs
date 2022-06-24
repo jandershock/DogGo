1be56b7f1cdfa3d61364f066ca2943b1d8c4ae35
jandershock <jordan.andershock@gmail.com>
2022-06-24 14:44:02 -0500

    Created basic custom helper tag

---
+++ DogGo/TagHelpers/CustomLabelTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace DogGo.TagHelpers
+{
+    public class CustomLabelTagHelper : TagHelper
+    {
+        public ModelExpression AspFor { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "label";
+            output.Attributes.SetAttribute("for", $"{AspFor.Name}");
+            output.Content.SetHtmlContent($"{AspFor.Metadata.GetDisplayName()}:");
+        }
+    }
+}
